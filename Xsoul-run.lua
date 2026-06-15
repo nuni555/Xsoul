3bb856ce6eaaa20bba64d457243f9380ab6ff04d
Anucha_nuni <148975549+nuni555@users.noreply.github.com>
2026-06-15 22:13:23 +0700

    Xsoul-Grow a garden2.lua

--- Xsoul-run.lua
+++ Xsoul-run.lua
@@ -4,7 +4,7 @@ local HttpGet = game.HttpGet
 local GameId: number = game.GameId
 
 local Games: {[number]: string} = loadstring(
-  HttpGet(game, "https://raw.githubusercontent.com/nuni555/Xsoul-FilesMenu/refs/heads/main/Xsoul_menu.lua")
+  HttpGet(game, "https://raw.githubusercontent.com/nuni555/Xsoul-FilesMenu/refs/heads/main/Xsoul_menu.lua?token=GHSAT0AAAAAAD776ZSCKNWS3OI62KMIWCAI2RQCY5A")
 )() :: any
 
 local URL: string? = Games[GameId]

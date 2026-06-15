db1489841ee19e14d5c87eabae90dfb86e89313a
Anucha_nuni <148975549+nuni555@users.noreply.github.com>
2026-06-15 22:04:38 +0700

    Xsoul-run.lua

--- Xsoul-run.lua
+++ Xsoul-run.lua
@@ -4,7 +4,7 @@ local HttpGet = game.HttpGet
 local GameId: number = game.GameId
 
 local Games: {[number]: string} = loadstring(
-  HttpGet(game, "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua")
+  HttpGet(game, "https://raw.githubusercontent.com/nuni555/Xsoul-FilesMenu/blob/a51500aa36507e51daef196e18b5a3da65d690d7/Xsoul_menu.lua")
 )() :: any
 
 local URL: string? = Games[GameId]
